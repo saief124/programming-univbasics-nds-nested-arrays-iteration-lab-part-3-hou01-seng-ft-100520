0071cdec7c37637c260cd258a156174c38e55774
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-18 04:29:05 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,10 +6,11 @@ row_index=0
 while row_index<src.count do
   element_index=0
   while element_index<src[row_index].count do
-  #  if src[row_index][element_index].instance_of?(String)
+   if src[row_index][element_index].instance_of?(String)
     total += src[row_index][element_index]
+  end
     element_index +=1
-  #  end
+  
   end
 row_index+=1
 end
